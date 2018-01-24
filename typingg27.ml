de1723afacfbbe58795e1e3ee613913ce303250c
LAPTOP-NG9F10JF\Salvatore <bobbore92@Gmail.com>
2018-01-24 17:45:12 +0100

    ahahah

--- typingg27.ml
+++ typingg27.ml
@@ -1,28 +1,42 @@
 (*typingg27.ml*)
 
-(*realizzare i tipi intero op variabile*)
+Type ide = string;;
 
-(*t1 + t2, t1 − t2, t1 × t2, t1 ∧ t2, t1 ∨ t2, t1 = t2,t1 < t2*)
-
-Type var = string of String;;(*da ricordarci di implementare la Let *)
-
-Type op = c
-		 piu of char
-		|meno of char
-		|per of char 
-		|not of char 
-		|uguale of char
-		|minore of char
-		| t1 ∧ t2 of bool | t1 ∨ t2 of bool;;
 		 
-Type x = a | int | bool | char;;
+type eval = None 
+          | Int of int 
+          | Bool of bool
 
-Type exp = x_a of x
-		  |op_a of exp * op * exp(*da ricorsarsi implementare l'errore op * op * op (+++) tipo aspettato exp * op * exp *)
-		  |var_a of var
-		  |let_a of var * exp * exp
-		  |liste of exp::expτ 
-		  |tupla of  epx * exp
-		  | τ1(τ2)
-		  |_ (*mettere messaggio di tipo non conosciuto*);;
+		  
+type exp = 
+			Eint of int 
+		  | Ebool of bool 
+		  | Den of ide
+		  | Prod of exp * exp
+		  | Sum of exp * exp
+		  | Diff of exp * exp
+		  | Eq of exp * exp
+		  | Minus of exp
+		  | Iszero of exp
+		  | Or of exp * exp
+		  | And of exp * exp
+		  | Not of exp
+		  | Ifthenelse of exp * exp * exp;;
+		  
+Type exp = X_a of eval
+		  |Plus of exp * exp
+		  |Menus of exp * exp
+		  |Mul of exp * exp 
+		  |Not of exp * exp 
+		  |Equals of exp * exp
+		  |Less of exp * exp
+		  |Hig of exp * exp
+		  |Or of exp * exp
+		  |And of exp * exp
+		  |Var of var
+		  |Let of var * exp * exp
+		  |Liste of exp::exp
+		  |Tupla of  epx * exp
+		  |Fun_a of exp(exp)
+		  |Not_a of exp;;
 		
