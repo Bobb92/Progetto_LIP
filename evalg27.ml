dfdc918c967edfd675febd617c97829b06487db8
LAPTOP-NG9F10JF\Salvatore <bobbore92@Gmail.com>
2018-01-27 18:09:33 +0100

    aggiunto altro da guardare

--- evalg27.ml
+++ evalg27.ml
@@ -1,84 +1,166 @@
 (* Operations on Eval *)
-(*Sto coppiando cose da dokuwiki per realizzare una base per il progetto, questa è la parte 2*)
 
-let typecheck (x, y) = match x with
+type ide = Ide of string;;
+
+type exp = Val of ide
+			| Eint of int
+			| Echar of char
+			| True
+			| False
+			| Empty
+			| Sum of exp * exp
+			| Diff of exp * exp
+			| Times of exp * exp
+			| And of exp * exp
+			| Or of exp * exp
+			| Not of exp
+			| Eq of exp * exp
+			| Less of exp * exp
+			| Cons of exp * exp
+			| Head of exp
+			| Tail of exp
+			| Fst of exp
+			| Snd of exp
+			| Epair of exp * exp
+			| Ifthenelse of exp * exp * exp
+			| Let of ide * exp * exp
+			| Fun of ide * exp
+			| Appl of exp * exp
+			| Rec of ide * exp ;;
+
+type eval =
+			Undefined
+			| Int of int
+			| Bool of bool
+			| Char of char
+			| List of eval list
+			| Pair of eval * eval
+			| Closure of exp * env
+			and
+			env = ide -> eval;;
+
+let typecheck (x, y) =
+      match x with
       | "int" ->
-		(match y with 
-		|  Int(u) -> true
-		| _ -> false)
+    (match y with 
+    |  Int(u) -> true
+    | _ -> false)
       | "bool" ->
-		(match y with 
-		|  Bool(u) -> true
-		| _ -> false)
-	  | "char" -> 
-		(match y with
-		| Char(u) -> true
-		| _ -> false)
-	  
+    (match y with 
+    |  Bool(u) -> true
+    | _ -> false)
+      | "char" -> 
+          (match y with
+          | Char(u) -> true
+          | _ -> false)
       | _ -> failwith ("not a valid type");;
 	  
+let Eq a = match a with
+		| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Bool(u = w))
+				  else if typecheck("char",x) & typecheck("char",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Bool(u = w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+		
+let Sum a = match a with 
+			| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Int(u + w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+		
+let Diff a = match a with 
+			| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Int(u - w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+		
+let Times a = match a with 
+			| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Int(u * w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+		
+let And a = match a with 
+			| (x,y) -> if typecheck("bool",x) & typecheck("bool",y) 
+					then 
+						(match (x,y) with
+						| (Bool(u), Bool(w)) -> Bool(u & w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+		
+let Or a = match a with 
+			| (x,y) -> if typecheck("bool",x) & typecheck("bool",y) 
+					then 
+						(match (x,y) with
+						| (Bool(u), Bool(w)) -> Bool(u | w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+
+let Not a = if typecheck("bool", a) 
+			then (if a then Bool(false) else Bool(true))
+			else failwith ("type error");;
+			
+let Less a = match a with 
+			| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Bool(u < w))
+				  else failwith ("type error")
+		| _ -> failwith ("argument error");;
+
+let Head a = match a with
+			| x::xl -> x
+			| _ -> failwith ("invalid list");;
+			
+let Fst a = match a with 
+			| (x,y) -> x
+			| _ -> failwith ("invalid tupla");;
+			
+let Snd a = match a with 
+			| (x,y) -> y
+			| _ -> failwith ("invalid tupla");;
+			
+let Cons a = match a with
+			| (x,y) -> if typecheck("int",x) & typecheck("int",y) 
+					then 
+						(match (x,y) with
+						| (Int(u), Int(w)) -> Int list(u :: w))
+				  else if typecheck("bool",x) & typecheck("bool",y) 
+					then 
+						(match (x,y) with
+						| (Bool(u), Bool(w)) -> Bool list(u :: w))
+				  else if typecheck("char",x) & typecheck("char",y) 
+					then 
+						(match (x,y) with
+						| (Char(u), Char(w)) -> Char list(u :: w))
+				  else failwith ("type error")
+			| _ -> failwith ("invalid ");;
+			
+let Tail a = match a with
+			| x::xl -> xl
+			| _ -> failwith ("invalid list");;
+			
+let Epair a =  match a with
+			| (x,y) -> (x,y)
+			| _ -> failwith ("invalid tupla");;
+
+
+
+		
+		
+		
+
  
-    let minus x =
-      if typecheck("int",x) 
-      then 
-    	(match x with
-    	| Int(y) -> Int(-y) )
-      else 
-    	failwith ("type error")
- 
-    let iszero x =
-      if typecheck("int",x) 
-      then 
-     	(match x with
-     	| Int(y) -> Bool(y=0) )
-      else 
-     	failwith ("type error")
- 
-    let equ (x,y) =
-      if typecheck("int",x) & typecheck("int",y) 
-      then 
-   	(match (x,y) with
-   	| (Int(u), Int(w)) -> Bool(u = w))
-      else failwith ("type error")
- 
-    let plus (x,y) =
-      if typecheck("int",x) & typecheck("int",y) 
-      then 
-   	(match (x,y) with
-   	| (Int(u), Int(w)) -> Int(u+w))
-      else failwith ("type error")
- 
-    let diff (x,y) =
-      if typecheck("int",x) & typecheck("int",y) 
-      then 
-   	(match (x,y) with
-   	| (Int(u), Int(w)) -> Int(u-w))
-      else failwith ("type error")
- 
-    let mult (x,y) =
-      if typecheck("int",x) & typecheck("int",y) 
-      then 
-   	(match (x,y) with
-   	| (Int(u), Int(w)) -> Int(u*w))
-      else failwith ("type error")
- 
-    let et (x,y) =
-      if typecheck("bool",x) & typecheck("bool",y) 
-      then 
-   	(match (x,y) with
-   	| (Bool(u), Bool(w)) -> Bool(u & w))
-      else failwith ("type error")
- 
-    let vel (x,y) =
-      if typecheck("bool",x) & typecheck("bool",y) 
-      then 
-   	(match (x,y) with
-   	| (Bool(u), Bool(w)) -> Bool(u or w))
-      else failwith ("type error")
- 
-    let non x =
-      if typecheck("bool",x) 
-      then 
-     	(match x with
-     	| Bool(y) -> Bool(not y) )
-      else failwith ("type error");;
+
+	  
--- typingg27.ml
+++ typingg27.ml
@@ -1,42 +1,59 @@
 (*typingg27.ml*)
 
-Type ide = string;;
+type ide = string ;;
 
 		 
 type eval = None 
           | Int of int 
           | Bool of bool
+		  | Char of char ;;
 
 		  
-type exp = 
-			Eint of int 
-		  | Ebool of bool 
-		  | Den of ide
-		  | Prod of exp * exp
-		  | Sum of exp * exp
-		  | Diff of exp * exp
-		  | Eq of exp * exp
-		  | Minus of exp
-		  | Iszero of exp
-		  | Or of exp * exp
-		  | And of exp * exp
-		  | Not of exp
-		  | Ifthenelse of exp * exp * exp;;
+type exp = Val of ide
+			| Eint of int
+			| Echar of char
+			| True
+			| False
+			| Empty
+			| Sum of exp * exp
+			| Diff of exp * exp
+			| Times of exp * exp
+			| And of exp * exp
+			| Or of exp * exp
+			| Not of exp
+			| Eq of exp * exp
+			| Less of exp * exp
+			| Cons of exp * exp
+			| Head of exp
+			| Tail of exp(*da fare nella semantica*)
+			| Fst of exp
+			| Snd of exp
+			| Epair of exp * exp(*da fare nella semantica*)
+			(*ahahahahah queste funzioni sono da capire e vedere come implementarle*)
+			| Ifthenelse of exp * exp * exp
+			| Let of ide * exp * exp
+			| Fun of ide * exp
+			| Appl of exp * exp
+			| Rec of ide * exp ;;
+			
+type env = ide -> eval;;
+
 		  
-Type exp = X_a of eval
-		  |Plus of exp * exp
-		  |Menus of exp * exp
-		  |Mul of exp * exp 
-		  |Not of exp * exp 
-		  |Equals of exp * exp
-		  |Less of exp * exp
-		  |Hig of exp * exp
-		  |Or of exp * exp
-		  |And of exp * exp
-		  |Var of var
-		  |Let of var * exp * exp
-		  |Liste of exp::exp
-		  |Tupla of  epx * exp
-		  |Fun_a of exp(exp)
-		  |Not_a of exp;;
+type eval = Undefined
+			| Int of int
+			| Bool of bool
+			| Char of char
+			| List of eval list
+			| Pair of eval * eval
+			| Closure of exp * env ;;
+			
+			
+type etype = TBool
+			| TInt
+			| Tchar
+			| TVar of string
+			| TPair of etype * etype
+			| TList of etype list
+			| TFun of etype * etype;;
+
 		
